c8fb43daa5ccc24c1942a4bdc48d12d669757bcc
Robin Shahan <robinsh@microsoft.com>
2016-08-22 14:06:34 -0700

    Add advanced class; codeflow reviews done.

---
+++ FileStorage/Common.cs
@@ -0,0 +1,62 @@
+﻿//----------------------------------------------------------------------------------
+// Microsoft Azure Storage Team
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+//----------------------------------------------------------------------------------
+// The example companies, organizations, products, domain names,
+// e-mail addresses, logos, people, places, and events depicted
+// herein are fictitious.  No association with any real company,
+// organization, product, domain name, email address, logo, person,
+// places, or events is intended or should be inferred.
+//----------------------------------------------------------------------------------
+
+
+using Microsoft.WindowsAzure.Storage;
+using System;
+
+namespace FileStorage
+{
+    public class Common
+    {
+        /// <summary>
+        /// Validates the connection string information in app.config and throws an exception if it looks like 
+        /// the user hasn't updated this to valid values. 
+        /// </summary>
+        /// <param name="storageConnectionString">The storage connection string</param>
+        /// <returns>CloudStorageAccount object</returns>
+        public static CloudStorageAccount CreateStorageAccountFromConnectionString(string storageConnectionString)
+        {
+            CloudStorageAccount storageAccount;
+            try
+            {
+                storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid storage account information provided. Please confirm the AccountName and AccountKey are valid in the app.config file - then restart the sample.");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadLine();
+                throw;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid storage account information provided. Please confirm the AccountName and AccountKey are valid in the app.config file - then restart the sample.");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadLine();
+                throw;
+            }
+
+            return storageAccount;
+        }
+
+        public static void WriteException(Exception ex)
+        {
+            Console.WriteLine("Exception thrown. {0}, msg = {1}", ex.Source, ex.Message);
+        }
+
+    }
+}
